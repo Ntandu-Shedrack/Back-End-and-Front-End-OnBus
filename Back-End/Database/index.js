8fd5ac8f5d1a107ada5ee78c9c6f9cf85fef09a8
Shedrack  Ntandu <107373308+Ntandu-Shedrack@users.noreply.github.com>
2024-06-13 10:22:38 +0300

    afsg

--- Back-End/Database/index.js
+++ Back-End/Database/index.js
@@ -1,20 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Buses = require('./models/buses.model.js');
 const app = express();
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+  });
 
 
-app.get('/', (req,res) => {
-    res.send("Hello from Node API");
-});
+app.post('/api/buses', async (req, res) => {
+    try {
+        const buses = await Buses.create(req.body);
+        res.status(200).json(buses);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
 
-mongoose.connect("mongodb+srv://josantashedrack:YBXMy5TZsoo7z7Bc@backend.d7mqpfu.mongodb.net/?retryWrites=true&w=majority&appName=Backend")
+
+mongoose.connect('mongodb+srv://josantashedrack:7UWbRmeOBStckCHy@backend.d7mqpfu.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Backend')
 .then(() => {
-    console.log("Connected to database");
+    console.log('Connected to database');
     app.listen(3000, () => {
-        console.log('server is running')
+        console.log('Hello there, server is running');
     });
 })
-.catch(() =>{
-    console.log("Connection failed!")
-})
+.catch(() => {
+    console.log('Connection failed');
+});
