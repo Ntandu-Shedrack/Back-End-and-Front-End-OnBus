0b8a93e92c2027169007e791ebaff309c46e2fed
Shedrack  Ntandu <107373308+Ntandu-Shedrack@users.noreply.github.com>
2024-06-13 12:45:25 +0000

    api added

--- Back-End/Database/index.js
+++ Back-End/Database/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Buses = require('./models/buses.model.js');
+const Buses = require('./models/buses.models');
 const app = express();
 
 app.use(express.json());
@@ -9,11 +9,33 @@ app.get('/', (req, res) => {
     res.send('Hello World');
   });
 
+app.get( 'api/buses', async (req, res) => {
+    try {
+        const buses = await Buses.find({});
+        res.status(200).json(buses);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+
+app.get( 'api/buses/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const buses = await Buses.findById(id);
+        res.status(200).json(buses);
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
 
 app.post('/api/buses', async (req, res) => {
     try {
         const buses = await Buses.create(req.body);
         res.status(200).json(buses);
+        
     } catch (error) {
         res.status(500).json({message: error.message});
     }
